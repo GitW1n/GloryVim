5121f53dee5e003effa2dbea2798d1106166898e
GitW1n <167911620+GitW1n@users.noreply.github.com>
2024-11-04 16:57:00 +0300

    Update config.lua

--- lua/core/config.lua
+++ lua/core/config.lua
@@ -1,10 +1,10 @@
 -- core/config.lua
-vim.opt.number = true                -- Показывать номера строк
-vim.opt.relativenumber = true        -- Относительная нумерация строк
-vim.opt.tabstop = 4                  -- Количество пробелов для табуляции
-vim.opt.shiftwidth = 4               -- Количество пробелов для автоотступов
-vim.opt.expandtab = true             -- Преобразование табов в пробелы
-vim.opt.termguicolors = true         -- Включить поддержку 24-битных цветов
+vim.opt.number = true                -- line numbers
+vim.opt.relativenumber = true        
+vim.opt.tabstop = 4                  
+vim.opt.shiftwidth = 4               
+vim.opt.expandtab = true             
+vim.opt.termguicolors = true         -- 24bit colors
 vim.o.mouse = 'a'
 vim.opt.encoding = "utf-8"
 vim.opt.cursorline = true
