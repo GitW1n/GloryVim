eddaa247b5b695e2c4d6ac66108c8c1fe6276250
GitW1n <167911620+GitW1n@users.noreply.github.com>
2024-11-04 16:57:33 +0300

    Update keymaps.lua

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -5,6 +5,6 @@ local set_keymap = vim.api.nvim_set_keymap
 local options = { noremap = true, silent = true }
 
 -- Пример горячих клавиш
-set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', options) -- Поиск файлов с Telescope
-set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', options)        -- Открыть/закрыть NvimTree
+set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', options) -- Search with Telescope
+set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', options)        -- Open/Close NvimTree
 
