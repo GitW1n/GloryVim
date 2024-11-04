11d275e84ee955c24693e44a4ea5b21237c64a6a
GitW1n <167911620+GitW1n@users.noreply.github.com>
2024-11-04 16:53:13 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
--- init.lua
-
 -- Убедитесь, что Packer загружен
 vim.cmd [[packadd packer.nvim]]
 
@@ -15,3 +13,25 @@ packer.startup(function(use)
     require('core.plugins')(use)  -- Вызываем функцию для установки плагинов
 end)
 
+-- Настройка dashboard-nvim
+require('core.dashboard').setup {
+    theme = 'hyper',  -- Опционально: выбираем тему
+    config = {
+        header = {
+            [[░██████╗░██╗░░░░░░█████╗░██████╗░██╗░░░██╗██╗░░░██╗██╗███╗░░░███╗]],
+            [[██╔════╝░██║░░░░░██╔══██╗██╔══██╗╚██╗░██╔╝██║░░░██║██║████╗░████║]],
+            [[██║░░██╗░██║░░░░░██║░░██║██████╔╝░╚████╔╝░╚██╗░██╔╝██║██╔████╔██║]],
+            [[██║░░╚██╗██║░░░░░██║░░██║██╔══██╗░░╚██╔╝░░░╚████╔╝░██║██║╚██╔╝██║]],
+            [[╚██████╔╝███████╗╚█████╔╝██║░░██║░░░██║░░░░░╚██╔╝░░██║██║░╚═╝░██║]],
+            [[░╚═════╝░╚══════╝░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░░░░╚═╝░░░╚═╝╚═╝░░░░░╚═╝]],
+        },
+        center = {
+            {icon = '  ', desc = 'Recently opened files  ', action = 'Telescope oldfiles', key = 'r'},
+            {icon = '  ', desc = 'Find file              ', action = 'Telescope find_files', key = 'f'},
+            {icon = '  ', desc = 'Find text              ', action = 'Telescope live_grep', key = 'g'},
+            {icon = '  ', desc = 'Open configuration      ', action = 'edit ~/.config/nvim/init.lua', key = 'c'},
+            {icon = '  ', desc = 'Exit                   ', action = 'exit', key = 'q'},
+        },
+        footer = {"Neovim - GloryVim"},
+    }
+}
