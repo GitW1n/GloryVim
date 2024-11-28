f8ae5e256738713c51dc7eb0be452328dcd0dc94
Yakov <167911620+GitW1n@users.noreply.github.com>
2024-11-28 22:53:04 +0300

    Update keymaps.lua

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -4,6 +4,5 @@
 local set_keymap = vim.api.nvim_set_keymap
 local options = { noremap = true, silent = true }
 
-
 set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', options) -- Search with Telescope
 set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', options)        -- Open/Close NvimTree
